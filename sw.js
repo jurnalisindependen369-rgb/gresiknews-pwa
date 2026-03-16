ea4bd27ae24a9d62bde2fcc84646d0427abbefe6
jurnalisindependen369-rgb <jurnalisindependen369@gmail.com>
2026-03-16 19:59:46 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,17 @@
-const CACHE = 'gresiknews-v1';
+const CACHE = 'gresiknews-v2';
+const OFFLINE_PAGE = 'https://www.gresiknews.id/';
+
 const ASSETS = [
   'https://www.gresiknews.id/',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css',
-  'https://cdn.jsdelivr.net/gh/jettheme/js@0.5.5/main.js'
+  'https://cdn.jsdelivr.net/gh/jettheme/js@0.5.5/main.js',
+  'https://jurnalisindependen369-rgb.github.io/gresiknews-pwa/icon-192.png',
+  'https://jurnalisindependen369-rgb.github.io/gresiknews-pwa/icon-512.png'
 ];
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
+    caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {}))
   );
   self.skipWaiting();
 });
@@ -23,15 +27,21 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   if(e.request.method !== 'GET') return;
+  if(e.request.url.indexOf('http') !== 0) return;
+
   e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
+    caches.match(e.request).then(cached => {
+      const fetchPromise = fetch(e.request).then(res => {
+        if(res && res.status === 200 && res.type === 'basic'){
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
         return res;
-      })
-      .catch(() =>
-        caches.match(e.request).then(r => r || caches.match('https://www.gresiknews.id/'))
-      )
+      }).catch(() => cached || caches.match(OFFLINE_PAGE));
+      return cached || fetchPromise;
+    })
   );
 });
+
+self.addEventListener('push', e => {
+  const d
